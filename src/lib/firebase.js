8c79ba4eb08c26dc1f342b6cf247795b2d2c441b
anshulmalikbudgetree <anshulmalik695@gmail.com>
2026-06-17 12:33:44 +0530

    Updated Contact Us section

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,8 +6,8 @@ apiKey: "AIzaSyCLHZ-8jD1rzL4C-dmrKc8yAQpDUg1Xmnk",
 authDomain: "ridesphere-1300e.firebaseapp.com",
  projectId: "ridesphere-1300e",
 storageBucket: "ridesphere-1300e.firebasestorage.app",
-messagingSenderld: "692969913071",
-appId: "1:692969913071:web: bc281f5875514c98ff91a",
+messagingSenderId: "692969913071",
+appId: "1:692969913071:web:bc281f5875514c98ff91a",
 measurementId: "G-NHK1EQM3L8"
 };
 
